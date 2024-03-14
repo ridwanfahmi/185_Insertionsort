d3b4ab161c58f68635a12595ab8c6cb6b33d76e3
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-03-14 11:01:46 +0700

    Menambahkan Procedure display

--- 185_D_InsertionSort/185_D_InsertionSort.cpp
+++ 185_D_InsertionSort/185_D_InsertionSort.cpp
@@ -57,3 +57,15 @@ void insertionsort() {
 	}
 }
 
+void display() {                //Procedure display
+	cout << endl;                                          //Output ke layar
+	cout << "\n=================================" << endl;             //Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;   //Output ke layar
+	cout << "=================================" << endl;   //Output ke layar
+
+	for (int j = 0; j < n; j++) {                          //Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;                            //Output ke layar
+	}
+	cout << endl;                                          //Output baris kosong
+}
+
