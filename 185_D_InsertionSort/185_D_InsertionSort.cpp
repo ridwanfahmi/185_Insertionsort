e0715a0ffb2d305500bef3f0e09f0ee95b3aa669
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-03-14 10:24:46 +0700

    Menambahkan Procedure input

--- 185_D_InsertionSort/185_D_InsertionSort.cpp
+++ 185_D_InsertionSort/185_D_InsertionSort.cpp
@@ -4,3 +4,22 @@ using namespace std;
 int arr[20];                            //Membuat Array dengan panjang data 20
 int n;                                  //Membuat Variable inputan n
 
+void input() {                          //procedure input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";  //Membuat Inputan jumlah element Array
+		cin >> n;                                      //Memanggil variable inputan n
+
+		if (n <= 20) {                                 //Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";   //Menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;                                          //Membuat jarak per baris program
+	cout << "======================" << endl;              //Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;              
+	cout << "======================" << endl;
+}
